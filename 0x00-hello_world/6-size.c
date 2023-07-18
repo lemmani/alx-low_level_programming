e6b10c30f89e2cf51c4e3fa0733d50edc0a8e24d
Emmanuel <lemmanuelf@gmail.com>
2023-07-17 18:23:29 -0700

    the one

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
-# include <stdio.h>
+#include<stdio.h>
 /**
 * main - Entry point
-*
 * Return: Always 0 (Success)
 */
 int main(void)
@@ -12,4 +11,4 @@ printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
-} 
+}
