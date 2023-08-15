54031cd1f0b7dd13ed7d58b9e707fcc89d510c4f
emmanuel <lemmanuelf@gmail.com>
2023-08-14 18:54:29 -0700

    the one

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+/*
+ *
+ *Another one
+ * 
+ */
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
+
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,11 @@
+/*
+ *
+ * another one
+ */
+#ifndef PI_H
+#define PI_H
+
+#define PI 3.14159265359
+
+#endif
+
