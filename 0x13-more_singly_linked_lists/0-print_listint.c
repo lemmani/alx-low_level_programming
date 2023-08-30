dadce47c767ceeac605b14826a1ff57a94429940
lemmani <lemmanuelf@gmail.com>
2023-08-29 17:08:54 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "list.h"
+
+/**
+ * print_listint - prints all the element of the linked list int he code
+ * @h: linked list of type listint_t to print in this code
+ * Return: number of nodes in the code
+ * lahai
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->v);
+		num++;
+		h = h->next;
+
+	}
+
+	return (num);
+
+}
