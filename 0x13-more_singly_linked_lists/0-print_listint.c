ffadb538ad03d0e56d943143682726752c514b81
lemmani <lemmanuelf@gmail.com>
2023-08-29 17:27:05 -0700

    the one

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,6 @@
  * print_listint - prints all the element of the linked list int he code
  * @h: linked list of type listint_t to print in this code
  * Return: number of nodes in the code
- * lahai
  */
 size_t print_listint(const listint_t *h)
 {
