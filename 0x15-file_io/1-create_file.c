983e40136d08a98149f358a075b9d17b3bb59763
lemmani <lemmanuelf@gmail.com>
2023-09-08 19:27:22 -0700

    the one

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,25 +3,26 @@
 /**
  * create_file - it creates a file.
  * @filename: Apointer to the name of the file to create
- * @text_context: A pointer to a string to write to the file
+ * @text_content: A pointer to a string to write to the file
  *
  * Return: if the function fails --1
  * otherwise 1.
  */
-int create_file(const char *filename, char *text_context)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_context != NULL)
+
+	if (text_content != NULL)
 	{
-		for (len = 0; text_context[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_context, len);
+	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
 		return (-1);
